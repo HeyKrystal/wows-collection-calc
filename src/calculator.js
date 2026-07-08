6fcd53e53765bdfba9cdf5019d34183cc2b0a1cb
HeyKrystal <5586868+HeyKrystal@users.noreply.github.com>
2026-07-08 15:17:34 -0500

    Initial Commit
    I'm essentially just being lazy and committing this mess to GitHub so I can easily download and fix it on my other computer.

---
+++ src/calculator.js
@@ -0,0 +1,15 @@
+export const CONSTANTS={
+SIMULATION_COUNT:500000,
+EXPECTED_RANGE_LOW_PERCENTILE:50,
+EXPECTED_RANGE_HIGH_PERCENTILE:90,
+MAX_COLLECTION_SIZE:256,
+MAX_ELEMENTS_PER_CONTAINER:100,
+MAX_DUPLICATE_RATE:1000
+};
+
+// Pure functions only:
+// simulateOneRun()
+// runSimulation()
+// calculateBestCase()
+// calculateWorstCase()
+// percentile()
---
+++ src/ui.js
@@ -0,0 +1,4 @@
+import { CONSTANTS } from "./calculator.js";
+
+// DOM wiring goes here.
+// Read inputs, call calculator.js, update outputs.
